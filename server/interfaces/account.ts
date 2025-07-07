fcaa5e23ffb36d488f0c7ac98e7eadc8f8d97322
Piero <piero7210@hotmail.com>
2025-07-07 15:17:28 -0500

    Interfaces docs

--- server/interfaces/account.ts
+++ server/interfaces/account.ts
@@ -1,6 +1,18 @@
+/**
+ * Interface representing a user account.
+ * 
+ * @interface IAccount
+ * @property {string} [_id] - Optional unique identifier for the account
+ * @property {string} name - First name of the account holder
+ * @property {string} [lastname] - Optional last name of the account holder
+ * @property {string} email - Email address associated with the account
+ * @property {string} [createdAt] - Optional timestamp when the account was created
+ * @property {string} [updatedAt] - Optional timestamp when the account was last updated
+ */
 export interface IAccount {
   _id?: string;
   name: string;
+  lastname?: string;
   email: string;
   createdAt?: string;
   updatedAt?: string;
--- server/interfaces/product.ts
+++ server/interfaces/product.ts
@@ -1,7 +1,21 @@
+/**
+ * Interface representing a product entity in the system.
+ * 
+ * @interface IProduct
+ * @property {string} [_id] - The unique identifier for the product, optional when creating a new product
+ * @property {string} name - The name of the product
+ * @property {string} sku - The stock keeping unit, a unique identifier for the product
+ * @property {number} stock - The current stock quantity of the product
+ * @property {string} [createdAt] - The timestamp when the product was created, optional
+ * @property {string} [updatedAt] - The timestamp when the product was last updated, optional
+ * @property {string} accountId - The identifier of the account that owns this product
+ */
 export interface IProduct {
   _id?: string;
   name: string;
   sku: string;
+  stock: number;
   createdAt?: string;
   updatedAt?: string;
+  accountId: string;
 }
